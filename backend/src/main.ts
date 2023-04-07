d532feff2f17172e7c0141306cba00d1c3aefb61
zake-dev <floatim00@gmail.com>
2023-04-07 17:37:56 +1000

    dev: initialise nest js app for backend
    Close #1

---
+++ backend/src/main.ts
@@ -0,0 +1,9 @@
+import configuration from '@/config/configuation';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(configuration().port);
+}
+bootstrap();
