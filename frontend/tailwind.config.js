4308c2e1285a4bc5633e60a16269f8044f318e40
zake-dev <floatim00@gmail.com>
2023-04-10 13:31:09 +1000

    feat: add color system

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('./src/styles/colors');
+
 export default {
   content: ['./index.html', './src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: colors,
+    },
   },
   plugins: [],
 };
