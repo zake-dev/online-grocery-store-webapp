65c401b331b4cd23ea8c9b2edbfaabdb0f3d5feb
zake-dev <floatim00@gmail.com>
2023-04-10 15:04:46 +1000

    feat: add typography system

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,9 @@ const colors = require('./src/styles/colors');
 export default {
   content: ['./index.html', './src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Pretendard Variable'],
+    },
     extend: {
       colors: colors,
     },
