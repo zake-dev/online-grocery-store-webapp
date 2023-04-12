54872d1eb20460d3a62159c2cb7f63f2a31359a3
zake-dev <floatim00@gmail.com>
2023-04-12 16:23:54 +1000

    feat: place order when user click the button

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,4 +3,5 @@ export * from './Modal';
 export { default as PaginationBar } from './PaginationBar';
 export * from './SideBar';
 export { default as SizedBox } from './SizedBox';
+export { default as SpinningLoader } from './SpinningLoader';
 export { default as TextInput } from './TextInput';
